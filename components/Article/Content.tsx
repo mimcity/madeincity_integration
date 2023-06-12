c069d9238cebbe9587a968905df7482a301b5007
mcagdas <mcagdasbayirli@gmail.com>
2023-06-12 14:14:57 +0300

    article responsive page

--- components/Article/Content.tsx
+++ components/Article/Content.tsx
@@ -7,8 +7,8 @@ import Badge from "../UI/Badge";
 const Content = () => {
   return (
     <main className="max-w-[1200px] mx-auto text-black w-full">
-      <div className="pt-20 pb-[200px] w-full">
-        <p className="text-xl text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
+      <div className="lg:pt-20 md:pt-16 pt-12 lg:pb-[200px] mb:pb-32 pb-[52px] w-full">
+        <p className="lg:text-xl md:text-lg text-base text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
           Après Rabat, c’est au tour de Marrakech de devenir la capitale
           culturelle du monde islamique ! Le ministre de la Jeunesse, de la
           Culture et de la Communication, Mohamed Mehdi Bensaïd, a annoncé cette
@@ -17,10 +17,10 @@ const Content = () => {
           événement est porté par l’Organisation du monde islamique pour
           l’éducation, les sciences et la culture (ISESCO).
         </p>
-        <h2 className="mt-12 text-3xl leading-[30px] font-semibold text-neutral-700 max-w-3xl mx-auto md:px-0 px-4">
+        <h2 className="mt-12 lg:text-3xl text-2xl leading-[30px] font-semibold text-neutral-700 max-w-3xl mx-auto md:px-0 px-4">
           Patrimoine mondial de l’UNESCO
         </h2>
-        <p className="mt-6 text-xl text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
+        <p className="lg:mt-6 md:mt-5 mt-4 lg:text-xl md:text-lg text-base text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
           Après Rabat, c’est au tour de Marrakech de devenir la capitale
           culturelle du monde islamique ! Le ministre de la Jeunesse, de la
           Culture et de la Communication, Mohamed Mehdi Bensaïd, a annoncé cette
@@ -39,13 +39,13 @@ const Content = () => {
             </span>
           </div>
         </div>
-        <h2 className="mt-[72px] text-3xl leading-[30px] font-semibold text-neutral-700 max-w-3xl mx-auto md:px-0 px-4">
+        <h2 className="lg:mt-[72px] md:mt-16 mt-14 lg:text-3xl text-2xl leading-[30px] font-semibold text-neutral-700 max-w-3xl mx-auto md:px-0 px-4">
           Des expos “by night”
         </h2>
-        <h3 className="mt-12 text-neutral-700 text-2xl leading-[30px] -tracking-wide max-w-3xl mx-auto md:px-0 px-4">
+        <h3 className="lg:mt-12 md:mt-8 mt-[22px] text-neutral-700 lg:text-2xl text-xl leading-[30px] -tracking-wide max-w-3xl mx-auto md:px-0 px-4">
           Au programme
         </h3>
-        <p className="mt-4 text-xl text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
+        <p className="md:mt-4 mt-3 lg:text-xl md:text-lg text-base text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
           20 espaces d’art provenant de différents pays dont la France, la Côte
           d’Ivoire, le Sénégal, l’Afrique du Sud ou encore l'Allemagne seront
           représentés et exposeront les œuvres de plus d’une soixantaine
@@ -53,10 +53,10 @@ const Content = () => {
           amateurs d’art. Le Royaume sera représenté par cinq galeries d’art,
           dont 3 établissements se trouvant à Casablanca.
         </p>
-        <h3 className="mt-12 text-neutral-700 text-2xl leading-[30px] -tracking-wide max-w-3xl mx-auto md:px-0 px-4">
+        <h3 className="mt-12 text-neutral-700 lg:text-2xl text-xl leading-[30px] -tracking-wide max-w-3xl mx-auto md:px-0 px-4">
           Le Centre culturel
         </h3>
-        <p className="mt-4 text-xl text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
+        <p className="md:mt-4 mt-3 lg:text-xl md:text-lg text-base text-neutral-700 leading-[30px] max-w-3xl mx-auto md:px-0 px-4">
           Situé au cœur de M Avenue, sera partenaire actif de la Foire. Tous les
           jours, la salle de théâtre diffusera le film retraçant les 10 ans de
           "1-54", à Paris, Londres, New York et Marrakech. En parallèle, il
@@ -66,11 +66,11 @@ const Content = () => {
           night” dont le solo show de Yasmina Alaoui au comptoir des Mines et
           l’ouverture de l’exposition “La saveur des couleurs” à la Galerie 38.
         </p>
-        <div className="max-w-3xl mx-auto py-[72px]">
+        <div className="max-w-3xl mx-auto lg:py-[72px] pt-[72px] pb-12 lg:px-0 px-4">
           <TopElementCard />
           <TopElementCard />
         </div>
-        <div className="flex items-center justify-between max-w-3xl mx-auto">
+        <div className="flex md:flex-row flex-col md:items-center justify-between max-w-3xl mx-auto md:px-0 px-4 md:gap-y-0 gap-y-4">
           <div>
             <p className="text-neutral-700 font-normal">26 Novembre 2023</p>
             <span className="text-neutral-500 text-xs">Par Anaïs Robin</span>
--- components/UI/TopElementCard.tsx
+++ components/UI/TopElementCard.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const TopElementCard = () => {
   return (
-    <div className="w-full py-[72px] border-b first:border-t">
-      <h2 className="text-3xl leading-[30px] font-semibold text-neutral-700">
+    <div className="w-full lg:py-[72px] md:py-16 py-12 border-b first:border-t">
+      <h2 className="lg:text-3xl text-xl leading-[30px] font-semibold text-neutral-700">
         Mandarin Oriental Marrakech
       </h2>
       <div className="mt-6">
@@ -16,7 +16,7 @@ const TopElementCard = () => {
         />
       </div>
       <div className="mt-6">
-        <p className="text-xl -tracking-wide text-neutral-700">
+        <p className="lg:text-xl md:text-lg text-base -tracking-wide text-neutral-700">
           Lorem ipsum dolor sit amet consectetur. Amet imperdiet egestas arcu
           in. Sollicitudin vitae proin suspendisse consequat accumsan nisl
           aliquam. Ut eleifend vulputate enim tortor phasellus at platea non
